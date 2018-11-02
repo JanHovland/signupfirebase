5c73ffd24e3d759313da57155ba149c036032c18
Jan Hovland <jan.hovland@lyse.net>
2018-11-02 15:38:05 +0100

    Initial Commit

---
+++ signupfirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signupfirebase
+//
+//  Created by Jan  on 02/11/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
