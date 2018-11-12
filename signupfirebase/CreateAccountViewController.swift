7be28f3eb5fa87b5d2c914cbe4df88c4356c62da
Jan Hovland <jan.hovland@lyse.net>
2018-11-12 14:10:33 +0100

    Oppdatering

---
+++ signupfirebase/CreateAccountViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateAccountViewController.swift
+//  signupfirebase
+//
+//  Created by Jan  on 12/11/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var eMailCreateAccount: UITextField!
+    @IBOutlet weak var nameCreateAccount: UITextField!
+    @IBOutlet weak var passwordCreateAccount: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
