bc461daa394f95bfec77d2767c1b5d257f0a2151
Jan Hovland <jan.hovland@lyse.net>
2018-11-15 22:06:50 +0100

    Update

--- signupfirebase/ViewController.swift
+++ signupfirebase/ViewController.swift
@@ -7,154 +7,16 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseAuth
-import CoreData
 
 class ViewController: UIViewController {
-
-var spinner = UIActivityIndicatorView()
-    
-    
-//    var isSignIn : Bool = true
-//
-//    var ePost : String = ""
-//    var passOrd : String = ""
     
     override func viewDidLoad() {
-        
-//        getData()
-//
-//        emailTextField.text = ePost
-//        passwordTextField.text = passOrd
-//
-//        // let navn = Auth.auth().currentUser!.displayName!
-//        // print(navn)
-
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-//        spinner.style = .gray
-//        spinner.hidesWhenStopped = true
-//        view.addSubview(spinner)
-//
-//        // Define layout constraints for the spinner
-//        spinner.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150.0),
-//                                     spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
-//
-//        // Activate the spinner
-//
-//        spinner.startAnimating()
-//
     }
+  
+    @IBOutlet weak var signInButton: UIButton!
     
-    override func viewDidAppear(_ animated: Bool) {
-//        spinner.stopAnimating()
-    }
-    
-//    @IBOutlet weak var signInSelector: UISegmentedControl!
-//    @IBOutlet weak var emailTextField: UITextField!
-//    @IBOutlet weak var passwordTextField: UITextField!
-      @IBOutlet weak var signInButton: UIButton!
-//    @IBAction func signInSelectorChanged(_ sender: Any) {
-//
-//        // Flip the boolean
-//        isSignIn = !isSignIn
-//
-//    }
-    
-//    @IBAction func signInButtonTapped(_ sender: UIButton) {
-//
-//
-//        // TODO: Do some validation on the email and password
-//        // print(emailTextField.text as Any)
-//        //
-//        // print(passwordTextField.text as Any)
-//
-//        print(isSignIn)
-//
-//        if let email = emailTextField.text, let pass = passwordTextField.text {
-//
-//            // Check if it is sign in or register
-//            if isSignIn {
-//                // Sign in the user with Firebase
-//
-//                Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
-//
-//                // Check that user isn't nil
-//
-//                if  user != nil {
-//                    // User is found, go to home screen
-//
-//                    // let eMail = self.emailTextField.text
-//
-//                    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-//                    let entity =  NSEntityDescription.entity(forEntityName: "Entity", in: context)
-//
-//                    let newEntity = NSManagedObject(entity: entity!, insertInto: context)
-//                    newEntity.setValue(self.emailTextField.text, forKey: "eMail")
-//                    newEntity.setValue(self.passwordTextField.text, forKey: "passWord")
-//
-//                    do {
-//                        try context.save()
-//                        print("Saved email and password")
-//                    } catch {
-//                        print("Failed saving")
-//                    }
-//
-////                     self.performSegue(withIdentifier: "goToUpdateUserData", sender: self)
-//                } else {
-//                    // Error: check error and show message
-//                }
-//
-//            }
-//
-//            } else {
-//                // Register the user with Firebase
-//
-//                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
-//                // Check that user isn't nil
-//
-//                    if  user != nil {
-//                        // User is found, go to home screen
-//
-//                        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-//                        changeRequest?.displayName = "Jan Hovland"
-//
-//                        changeRequest?.commitChanges { error in
-//                            if error == nil {
-//                                print("User display name changed!")
-//                                self.dismiss(animated: false, completion: nil)
-//                            } else {
-//                                print("Error: \(error!.localizedDescription)")
-//                            }
-//                        }
-//
-////                        self.performSegue(withIdentifier: "goToUpdateUserData", sender: self)
-//                    }
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        // Dismiss the keyboard when the view is tapped on
-//        emailTextField.resignFirstResponder()
-//        passwordTextField.resignFirstResponder()
-//    }
-//
-//    @IBAction func cancelPost(_ sender: Any) {
-//
-//        // Dismiss the popover
-//        presentingViewController?.dismiss(animated: true, completion: nil)
-//
-//
-//    }
-//
 }
 
 
