e03c7e3cf5cebfe2a5bbdfec4a424e02808af3e5
Jan Hovland <jan.hovland@lyse.net>
2018-11-22 13:03:00 +0100

    Update

---
+++ signupfirebase/ResetPasswordByMailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  UpdatePasswordByMailViewController.swift
+//  signupfirebase
+//
+//  Created by Jan  on 22/11/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordByMailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+//    Send a password reset email
+//
+//    You can send a password reset email to a user with the sendPasswordResetWithEmail:completion: method. For example:
+//
+//    SWIFTOBJECTIVE-C
+//    Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+//    // ...
+//    }
+//    You can customize the email template that is used in Authentication section of the Firebase console, on the Email Templates page. See Email Templates in Firebase Help Center.
+//
+//    It is also possible to pass state via a continue URL to redirect back to the app when sending a password reset email.
+//
+//    Additionally you can localize the password reset email by updating the language code on the Auth instance before sending the email. For example:
+//
+//    SWIFTOBJECTIVE-C
+//    Auth.auth().languageCode = "fr"
+//    // To apply the default app language instead of explicitly setting it.
+//    // Auth.auth().useAppLanguage()
+//    You ca
+
+}
