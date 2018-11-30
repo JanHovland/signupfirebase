c4a8d7f6f3a2943bac5fd492e51fa9b29b8ba622
Jan Hovland <jan.hovland@lyse.net>
2018-11-30 14:12:00 +0100

    Update

---
+++ signupfirebase/SetTheResetPasswordInCoreDataViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SetTheResetPasswordInCoreDataViewController.swift
+//  signupfirebase
+//
+//  Created by Jan  on 30/11/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class SetTheResetPasswordInCoreDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+    @IBAction func ResetPasswordInCoreData(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    
+}
