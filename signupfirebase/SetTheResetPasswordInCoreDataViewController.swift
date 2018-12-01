d08808ed34da4f46eb6e848f2328731e9c957376
Jan Hovland <jan.hovland@lyse.net>
2018-12-01 17:57:24 +0100

    Update

--- signupfirebase/SetTheResetPasswordInCoreDataViewController.swift
+++ signupfirebase/SetTheResetPasswordInCoreDataViewController.swift
@@ -6,18 +6,47 @@
 //  Copyright © 2018 Jan . All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class SetTheResetPasswordInCoreDataViewController: UIViewController {
+    var myTimer: Timer!
+
+    @IBOutlet var activity: UIActivityIndicatorView!
+    @IBOutlet var ResetPassword: UITextField!
+
+    // Setter en "constant" forsinkelse etter at en trykker på "Save"
+    let forsinkelse = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-    }
-  
     @IBAction func ResetPasswordInCoreData(_ sender: UIBarButtonItem) {
-        
-        
+        if ResetPassword.text!.count > 0 {
+            activity.startAnimating()
+
+            // Oppdaterer passordet i CoreData
+            let ok = updatePasswordCoreData(withEpost: (Auth.auth().currentUser?.email!)!,
+                                            withPassWord: ResetPassword.text!)
+
+            if ok == false {
+                presentAlert(withTitle: "Feil", message: "Kan ikke oppdatere passordet til brukeren i CoreData.")
+            }
+
+            // Legg inn en liten forsinkelse før funksjonen "returnToLogin" kalles
+            myTimer = Timer.scheduledTimer(timeInterval: TimeInterval(forsinkelse),
+                                           target: self,
+                                           selector: #selector(returnToLogin),
+                                           userInfo: nil,
+                                           repeats: false)
+            activity.stopAnimating()
+        } else {
+        }
     }
-    
+
+    @objc func returnToLogin() {
+        performSegue(withIdentifier: "BackToLoginViewControllerFromResetPasswordInCoreDara", sender: self)
+        myTimer.invalidate()
+    }
 }
