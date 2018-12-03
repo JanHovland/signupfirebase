5d2a0b568d6405d8728785291ef10d6b33fb4199
Jan Hovland <jan.hovland@lyse.net>
2018-12-03 16:57:08 +0100

    Update

--- signupfirebase/ListTableViewController.swift
+++ signupfirebase/ListTableViewController.swift
@@ -1,32 +1,24 @@
 //
-//  ListViewController.swift
+//  ListTableViewController.swift
 //  signupfirebase
 //
-//  Created by Jan  on 02/12/2018.
+//  Created by Jan  on 03/12/2018.
 //  Copyright © 2018 Jan . All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+class ListTableViewController: UITableViewController {
+
     var listItems = [NSManagedObject]()
-    
-    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // For å få dette til å virke: må @IBOutlet weak var tableView: UITableView! være aktiv
         tableView.delegate = self
         tableView.dataSource = self
 
-        // Alternativ måte er
-        // Ctrl drag from TableView til den øverste gule iconen
-        // Merk av delegate og dataSource
-        // Da trengs ikke:  @IBOutlet weak var tableView: UITableView!
-        
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
         
@@ -37,24 +29,26 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
         catch {
             print("Error")
         }
- 
+        
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listItems.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CoreDataTableViewCell
-
+        
         // Configure the cell...
-
+        
         let item = listItems[indexPath.row]
-
+        
         cell.uidLabel?.text = item.value(forKey: "uid") as? String
         cell.mailLabel?.text = item.value(forKey: "email") as? String
         cell.nameLabel?.text = item.value(forKey: "name") as? String
-
+        
         print(cell.uidLabel?.text as Any)
         
         
@@ -75,4 +69,3 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
 
 }
-
