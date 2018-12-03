40ff5738740e94d9afb76bff71b9ef43837aeff2
Jan Hovland <jan.hovland@lyse.net>
2018-12-03 13:59:51 +0100

    Update

---
+++ signupfirebase/CoreDataTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataTableViewCell.swift
+//  signupfirebase
+//
+//  Created by Jan  on 02/12/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class CoreDataTableViewCell: UITableViewCell {
+
+    @IBOutlet var uidLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ signupfirebase/ListViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ListViewController.swift
+//  signupfirebase
+//
+//  Created by Jan  on 02/12/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+//How To Create Custom Tableview Cells In Xcode 8 (Swift 3.0)
+
+
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var listItems = [NSManagedObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do {
+            let result = try context.fetch(request)
+            listItems = result as! [NSManagedObject]
+        }
+        catch {
+            print("Error")
+        }
+ 
+//        tableView.delegate = self
+//        ListViewController.datasource = self
+//       
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CoreDataTableViewCell
+
+        // Configure the cell...
+
+        let item = listItems[indexPath.row]
+
+        cell.uidLabel?.text = item.value(forKey: "uid") as? String
+        cell.mailLabel?.text = item.value(forKey: "email") as? String
+        cell.nameLabel?.text = item.value(forKey: "name") as? String
+
+        print(cell.uidLabel?.text as Any)
+        
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do {
+            let result = try context.fetch(request)
+            listItems = result as! [NSManagedObject]
+        }
+        catch {
+            print("Error")
+        }
+    }
+
+}
+
