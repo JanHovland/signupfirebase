b652a35af961161f473e2dd03997bf270b0a10e5
Jan Hovland <jan.hovland@lyse.net>
2018-12-05 10:55:08 +0100

    Bruker nå TableViewController for å liste ut alle brukerne

--- signupfirebase/ListTableViewController.swift
+++ signupfirebase/ListTableViewController.swift
@@ -1,58 +1,62 @@
 //
-//  ListViewController.swift
+//  ListTableViewController.swift
 //  signupfirebase
 //
-//  Created by Jan  on 02/12/2018.
+//  Created by Jan  on 05/12/2018.
 //  Copyright © 2018 Jan . All rights reserved.
 //
 
-import UIKit
 import CoreData
+import UIKit
 
-class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+class ListTableViewController: UITableViewController {
+    @IBOutlet var activity: UIActivityIndicatorView!
     var listItems = [NSManagedObject]()
-    
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var activity: UIActivityIndicatorView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         activity.hidesWhenStopped = true
         activity.style = .gray
         view.addSubview(activity)
 
         activity.startAnimating()
-        
+
+        // Dette er ikke nødvendig når class er subclass av UITableViewController
+
         // For å få dette til å virke: må @IBOutlet weak var tableView: UITableView! være aktiv
-        tableView.delegate = self
-        tableView.dataSource = self
+        //        tableView.delegate = self
+        //        tableView.dataSource = self
 
         // Alternativ måte er
         // Ctrl drag from TableView til den øverste gule iconen
         // Merk av delegate og dataSource
         // Da trengs ikke:  @IBOutlet weak var tableView: UITableView!
-        
+
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
-        
+
         do {
             let result = try context.fetch(request)
             listItems = result as! [NSManagedObject]
-        }
-        catch {
+        } catch {
             print("Error")
         }
- 
+
         activity.stopAnimating()
-  }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listItems.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CoreDataTableViewCell
 
         // Configure the cell...
@@ -64,28 +68,22 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
         cell.nameLabel?.text = item.value(forKey: "name") as? String
 
         print(cell.uidLabel?.text as Any)
-        
-        
+
         return cell
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
-        
         activity.startAnimating()
-        
+
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
-        
+
         do {
             let result = try context.fetch(request)
             listItems = result as! [NSManagedObject]
-        }
-        catch {
+        } catch {
             print("Error")
         }
         activity.stopAnimating()
-        
     }
-
 }
-
