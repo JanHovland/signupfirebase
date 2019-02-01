6d65b8d19e1be79da4fc7ca1a27d1ef9a7dfd052
Jan Hovland <jan.hovland@lyse.net>
2019-02-01 18:46:14 +0100

    Update

--- signupfirebase/SavePostalCodesFirebaseTableViewController.swift
+++ signupfirebase/SavePostalCodesFirebaseTableViewController.swift
@@ -118,6 +118,76 @@ class SavePostalCodesFirebaseTableViewController: UITableViewController, UIDocum
         // savePostalCodesFiredata(postnummer: "0001",
         //                         poststed: "Oslo")
 
+        // Load the CSV readString and parse it
+        
+        
+        //        Postnummer;Poststed;Kommunenummer;Kommunenavn;Kategori
+        //        0001;OSLO;0301;OSLO;P
+        //
+        
+        let delimiter = ";"
+        var items:[(Postnummer:String, Poststed:String, Kommunenummer: String, Kommunenavn: String, Kategori: String)]?
+        
+        
+        do {
+            let content = try String(contentsOf: sandboxFileURL)
+            items = []
+            let lines: [String] = content.components(separatedBy: .newlines)
+            
+            for line in lines {
+                var values:[String] = []
+                if line != "" {
+                    // For a line with double quotes
+                    // we use NSScanner to perform the parsing
+                    if line.range(of: "\"") != nil {
+//                        var textToScan:String = line
+//                        var value:NSString?
+//                        var textScanner:Scanner = Scanner(string: textToScan)
+//                        while textScanner.string != "" {
+//                            
+//                            if (textScanner.string as NSString).substring(to: 1) == "\"" {
+//                                textScanner.scanLocation += 1
+//                                textScanner.scanUpTo("\"", into: &value)
+//                                textScanner.scanLocation += 1
+//                            } else {
+//                                textScanner.scanUpTo(delimiter, into: &value)
+//                            }
+//                            
+//                            // Store the value into the values array
+//                            if let value = value {
+//                                values.append(value as String)
+//                            }
+//                            
+//                            // Retrieve the unscanned remainder of the string
+//                            if textScanner.scanLocation < textScanner.string.count {
+//                                textToScan = (textScanner.string as NSString).substring(from: textScanner.scanLocation + 1)
+//                            } else {
+//                                textToScan = ""
+//                            }
+//                            textScanner = Scanner(string: textToScan)
+//                        }
+//                        
+                        // For a line without double quotes, we can simply separate the string
+                        // by using the delimiter (e.g. comma)
+                    } else  {
+                        values = line.components(separatedBy: delimiter)
+                    }
+                    
+                    // Put the values into the tuple and add it to the items array
+                    let item = (Postnummer: values[0], Poststed: values[1], Kommunenummer: values[2], Kommunenavn: values[3], Kategori: values[4])
+                    
+                    print(item.Postnummer + " " + item.Poststed)
+                    
+                    items?.append(item)
+                }
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        
+        
         
         
         
