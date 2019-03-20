c306e184e4dfd102b575ca2d8c011db7d8bcfdde
Jan Hovland <jan.hovland@lyse.net>
2019-03-20 09:28:50 +0100

    Update

--- signupfirebase/UpdateUserNameViewController.swift
+++ signupfirebase/UpdateUserNameViewController.swift
@@ -12,8 +12,8 @@ import UIKit
 class UpdateUserNameViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var activity: UIActivityIndicatorView!
     @IBOutlet var NewNameTextField: UITextField!
-    @IBOutlet weak var userInfo: UILabel!
     @IBOutlet weak var OldNameTextField: UITextField!
+    @IBOutlet weak var forEmailTextField: UITextField!
     
     var myTimer: Timer!
     var activeField: UITextField!
@@ -23,29 +23,25 @@ class UpdateUserNameViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.NewNameTextField.delegate = self
+        // Turn off keyboard when you press "Return"
+        NewNameTextField.delegate = self
+        OldNameTextField.delegate = self
+        forEmailTextField.delegate = self
         
-        showUserInformation()
-
-        /// Turn off keyboard when you press "Return"
-        self.NewNameTextField.delegate = self
-        self.OldNameTextField.delegate = self
-
         activity.hidesWhenStopped = true
         activity.style = .gray
-        view.addSubview(activity)
-
+  
         activity.startAnimating()
 
-        OldNameTextField.text = Auth.auth().currentUser?.displayName
-
+        DispatchQueue.main.async {
+            self.OldNameTextField.text = Auth.auth().currentUser?.displayName
+            self.forEmailTextField.text = Auth.auth().currentUser?.email
+        }
+        
         activity.stopAnimating()
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        activity.startAnimating()
-        userInfo.text = showUserInfo(startUp: false)
-        activity.stopAnimating()
         // Observe keyboard change
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeUpdateName(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeUpdateName(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -94,41 +90,43 @@ class UpdateUserNameViewController: UIViewController, UITextFieldDelegate {
         
         NewNameTextField.resignFirstResponder()
         OldNameTextField.resignFirstResponder()
-        
+
         if (NewNameTextField.text?.count)! > 0 {
+            
             activity.startAnimating()
 
-            let region = NSLocale.current.regionCode?.lowercased()  // Returns the local region
-            Auth.auth().languageCode = region!
+            DispatchQueue.main.async {
+            
+                let region = NSLocale.current.regionCode?.lowercased()  // Returns the local region
+                Auth.auth().languageCode = region!
 
-            // Store the new username in Firebase
-            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-            changeRequest?.displayName = NewNameTextField.text
-            changeRequest?.commitChanges { error in
+                // Store the new username in Firebase
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.displayName = self.NewNameTextField.text
+                changeRequest?.commitChanges { error in
 
-                if error == nil {
-                    self.OldNameTextField.text = self.NewNameTextField.text
-                } else {
-                    // Handle error
-                    self.presentAlert(withTitle: NSLocalizedString("Error",
-                                                                   comment: "UpdateUserNameViewVontroller.swift SaveNewName "),
-                                      message: error?.localizedDescription as Any)
-                    }
+                    if error == nil {
+                        self.OldNameTextField.text = self.NewNameTextField.text
+                    } else {
+                        // Handle error
+                        self.presentAlert(withTitle: NSLocalizedString("Error",
+                                                                       comment: "UpdateUserNameViewVontroller.swift SaveNewName "),
+                                          message: error?.localizedDescription as Any)
+                        }
+                }
+
+                // Store the new username in CoreData
+                let ok = self.updateNameCoreData(withEpost: (Auth.auth().currentUser?.email)!, withNavn: self.NewNameTextField.text!)
+
+                if ok == false {
+                    let melding = "Unable to update the username in CoreData."
+                    self.presentAlert(withTitle: NSLocalizedString("Error", comment: "UpdateUserNameViewVontroller.swift SaveNewName "),
+                                 message: melding)
+                }
+
             }
-
-            // Store the new username in CoreData
-            let ok = updateNameCoreData(withEpost: (Auth.auth().currentUser?.email)!, withNavn: NewNameTextField.text!)
-
-            if ok == false {
-                let melding = "Unable to update the username in CoreData."
-                presentAlert(withTitle: NSLocalizedString("Error", comment: "UpdateUserNameViewVontroller.swift SaveNewName "),
-                             message: melding)
-            }
-
+            
             activity.stopAnimating()
-
-            // Insert a short delay before the function "returnToSettings" is called
-            myTimer = Timer.scheduledTimer(timeInterval: TimeInterval(forsinkelse), target: self, selector: #selector(showUserInformation), userInfo: nil, repeats: false)
 
         } else {
             let melding = NSLocalizedString("The username must have a value.", comment: "UpdateUserNameViewVontroller.swift SaveNewName ")
@@ -137,11 +135,6 @@ class UpdateUserNameViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
-    @objc func showUserInformation() {
-        userInfo.text = "" 
-    }
-
-    
     override func viewWillDisappear(_ animated: Bool) {
         // Remove observers
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
