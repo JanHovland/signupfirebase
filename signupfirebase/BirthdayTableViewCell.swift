44fc87755d2359d2c40ba69dc1f116b86d27f93b
Jan Hovland <jan.hovland@lyse.net>
2019-04-27 10:00:18 +0200

    Update

---
+++ signupfirebase/BirthdayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BirthdayTableViewCell.swift
+//  signupfirebase
+//
+//  Created by Jan Hovland on 27/04/2019.
+//  Copyright © 2019 Jan . All rights reserved.
+//
+
+import UIKit
+
+class BirthdayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
